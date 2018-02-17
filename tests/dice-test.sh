14db22172006d16399902ea7a34a510e09f19254
Timothy Rice <timothy.rice@unimelb.edu.au>
2018-02-18 03:59:48 +1100

    Add valgrind test if possible.

--- tests/dice-test.sh
+++ tests/dice-test.sh
@@ -3,8 +3,12 @@
 ./dice <<< 3d6
 ./dice <<< "5x 7d8 + 23"
 ./dice ./tests/testing.dice
+hash valgrind 2> /dev/null \
+    && valgrind -v --tool=memcheck --leak-check=full --show-leak-kinds=all --track-origins=yes \
+    dice ./tests/testing.dice
 
-for att in STR DEX CON INT WIS CHA; do
+hash datamash 2>/dev/null \
+&& for att in STR DEX CON INT WIS CHA; do
     printf "%s: " $att
     echo "4x d6" | ./dice | xargs -n1 | sort | tail --lines=+2 | datamash sum 1
 done
