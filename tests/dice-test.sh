4d8596af94b28d972969937223b5cac8e717483a
Timothy Rice <timothy.rice@unimelb.edu.au>
2018-02-27 07:35:36 +1100

    Warn when software for tests is missing.

--- tests/dice-test.sh
+++ tests/dice-test.sh
@@ -3,10 +3,15 @@
 ./dice <<< 3d6
 ./dice <<< "5x 7d8 + 23"
 ./dice ./tests/testing.dice
+
+hash valgrind 2> /dev/null \
+    || 1>&2 echo "Valgrind not found"
 hash valgrind 2> /dev/null \
     && valgrind -v --tool=memcheck --leak-check=full --show-leak-kinds=all --show-reachable=no --track-origins=yes \
     dice ./tests/testing.dice
 
+hash datamash 2> /dev/null \
+    || 1>&2 echo "Datamash not found"
 hash datamash 2>/dev/null \
 && for att in STR DEX CON INT WIS CHA; do
     printf "%s: " $att
