e188f836c6d04ae6a9d5d5c676cf4a20ae5dea53
Timothy Rice <timothy.rice@unimelb.edu.au>
2019-04-13 06:31:51 +1000

    Add more paths for valgrind to follow

--- tests/valgrind.sh
+++ tests/valgrind.sh
@@ -7,4 +7,4 @@ hash valgrind 2> /dev/null \
     }
 hash valgrind 2> /dev/null \
     && valgrind -v --tool=memcheck --leak-check=full --show-leak-kinds=all --show-reachable=no --track-origins=yes \
-    ./dice <<< "5x d4 + 2 + d6"
+    ./dice <<< "5x d4 + 2 + d6; 4d6!k3; 10x d2 T2;"
