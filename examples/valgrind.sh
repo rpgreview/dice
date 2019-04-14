4ad887595aa84541feb2ec1aa6173b1b69492c69
Timothy Rice <timothy.rice@unimelb.edu.au>
2019-04-14 11:15:02 +1000

    Turn current so-called 'tests' into examples

---
+++ examples/valgrind.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+hash valgrind 2> /dev/null \
+    && valgrind -v --tool=memcheck --leak-check=full --show-leak-kinds=all --show-reachable=no --track-origins=yes \
+    dice <<< "5x d4 + 2 + d6; 4d6!k3; 10x d2 T2;"
